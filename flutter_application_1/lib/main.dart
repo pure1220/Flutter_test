80a3b421b5b1d7355c5a7af9679951f9f0e7f9a5
S009497 <s009497@dgyiheda.com>
2024-02-28 15:30:24 +0800

    新增

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,285 @@
+/*
+ * @Description: main.dark应用入口文件
+ * @Version: 1.0
+ * @Author: hjc<09497,13229482412>
+ * @Date: 2024-02-22 09:35:04
+ * @LastEditors: hjc<09497,13229482412>
+ * @LastEditTime: 2024-02-28 15:03:16
+ * @FilePath: \test_drive\flutter_application_1\lib\main.dart
+ */
+
+// 导入Material Ui组件库
+
+import 'package:flutter/material.dart';
+
+// 应用入口函数 负责创建和运行应用 runApp()函数调用
+// 创建MyApp常量对象 作为参数传递
+void main() {
+  runApp(const MyApp());
+}
+
+// super关键字是用来调用父类的成员 super
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 返回描述信息 materialApp是基于mUi风格的，提供配置项
+    return MaterialApp(
+      // 标题
+      title: 'Flutter Demo',
+      // 主题
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // 首页
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // 常量 意味在编译时 就能确定值
+  // required 必须 this.title是必须的 如果缺失 会报错
+  const MyHomePage({super.key});
+
+  @override
+  // 创建相关联的状态对象
+  State<MyHomePage> createState() => _MyHomePageState();
+  // State<MyHomePage> createState() => _MyHomePageState();
+}
+
+// 顶部搜索栏
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+        child: Row(
+          children: <Widget>[
+            //Expanded
+            Expanded(
+                child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0, vertical: 4.0),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: const Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(Icons.search, color: Colors.white),
+                        SizedBox(width: 8.0),
+                        Text('搜索一下',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                                decoration: TextDecoration.none))
+                      ],
+                    ))),
+            const SizedBox(width: 16.0),
+            const Icon(Icons.auto_awesome_mosaic_outlined)
+          ],
+        ));
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Container(
+      color: Colors.white,
+      child: const Column(
+        children: [
+          SearchBarWidget(),
+          ContentWidget(),
+          BottomNavigationBarWidget()
+        ],
+      ),
+    ));
+  }
+}
+
+// 中间面板内容
+class ContentWidget extends StatefulWidget {
+  const ContentWidget({super.key});
+
+  @override
+  State<ContentWidget> createState() => _ContentWidgetState();
+}
+
+class _ContentWidgetState extends State<ContentWidget>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+
+  List<String> tabStrList = [
+    '关注',
+    '推荐',
+    '热榜',
+    '头条',
+    '后端',
+    '前端',
+    'Android',
+    'iOS',
+    '人工智能',
+    '开发工具'
+  ];
+
+  @override
+  // 初始化状态
+  void initState() {
+    super.initState();
+    _controller = TabController(length: tabStrList.length, vsync: this);
+  }
+
+  @override
+  // 销毁
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold填满剩余空间
+    return Expanded(
+        child: Scaffold(
+            body: Column(children: <Widget>[
+      TabBar(
+        tabs: tabStrList.map((e) => Tab(text: e)).toList(),
+        isScrollable: true, //设置可滚动
+        controller: _controller,
+      ),
+      // 高度填满剩余空间
+      Expanded(
+          child: TabBarView(
+        controller: _controller,
+        children:
+            tabStrList.map((e) => ContentListWidget(name: '$e title')).toList(),
+      ))
+    ])));
+  }
+}
+
+// 中间列表Widget
+class ContentListWidget extends StatefulWidget {
+  const ContentListWidget({super.key, required this.name});
+  final String name;
+
+  @override
+  State<ContentListWidget> createState() => _ContentListWidgetState();
+}
+
+class _ContentListWidgetState extends State<ContentListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        itemBuilder: (context, index) {
+          return Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+              child: Column(children: [
+                Row(children: [
+                  Text(widget.name,
+                      style: const TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold)),
+                ]),
+                const Row(children: [
+                  Icon(Icons.person, size: 20),
+                  SizedBox(width: 5),
+                  Text('pure_big')
+                ]),
+                const SizedBox(height: 6),
+                const Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                      '前端工程化这个词，是国内前端圈子2018年前后才出现的，大概的意思是将（后端已经比较成熟的）许多软件工程概念、实践、工具引入前端开发，提升开发效率。',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ))
+                  ],
+                ),
+                const SizedBox(height: 6),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.thumb_up,
+                      size: 16,
+                    ),
+                    const SizedBox(width: 5),
+                    const Text('999+'),
+                    const SizedBox(width: 10),
+                    const Icon(
+                      Icons.keyboard_voice,
+                      size: 16,
+                    ),
+                    const Text('999+'),
+                    // 空间组件 利用剩余空间进行占位
+                    const Spacer(),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 4.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: const Text(
+                        'Flutter',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    const Icon(
+                      Icons.more_horiz,
+                      size: 16,
+                    )
+                  ],
+                )
+              ]));
+        },
+        separatorBuilder: (context, index) {
+          return const Divider(
+            height: 1.0,
+            color: Colors.grey,
+          );
+        },
+        itemCount: 20);
+  }
+}
+
+// 底部导航栏
+class BottomNavigationBarWidget extends StatefulWidget {
+  const BottomNavigationBarWidget({super.key});
+
+  @override
+  State<BottomNavigationBarWidget> createState() =>
+      _BottomNavigationBarWidgetState();
+}
+
+class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.deepPurple,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: '首页'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: '搜索'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: '我的'),
+      ],
+      onTap: (position) {
+        setState(() {
+          _selectedIndex = position;
+        });
+      },
+    );
+  }
+}
---
+++ flutter_application_1/lib/main01.dart
@@ -0,0 +1,127 @@
+/*
+ * @Description: main.dark应用入口文件
+ * @Version: 1.0
+ * @Author: hjc<09497,13229482412>
+ * @Date: 2024-02-22 09:35:04
+ * @LastEditors: hjc<09497,13229482412>
+ * @LastEditTime: 2024-02-27 17:38:53
+ * @FilePath: \test_drive\flutter_application_1\lib\main01.dart
+ */
+
+// 导入Material Ui组件库
+import 'package:flutter/material.dart';
+
+// 相对路径导入
+// import '../test/widget_test.dart';
+
+// 支持使用别名进行空间命名
+// import 'package:http/http.dart' as http;
+
+// 应用入口函数 负责创建和运行应用 runApp()函数调用
+// 创建MyApp常量对象 作为参数传递
+// void 表示函数没有返回值
+void main() {
+  runApp(const MyApp());
+}
+
+// StatelessWidget无状态的小部件 静态
+// StatefulWidget有状态的小部件 动态
+// super关键字是用来调用父类的成员 super
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+// @override是Dart语言注解，用来提供元数据，提供类、方法、变量
+// 在面向对象编程，子类可以重写父类的方法以提供自己的实现方式，当使用注解时，他可以确保我正在重写父类的方法，而不是意外创建
+  @override
+
+  // Widget是Flutter应用中构建UI基本构建块
+  // build方法 是用来构建界面的
+  // BuildContent 参数  context 命名
+  Widget build(BuildContext context) {
+    // 返回描述信息 materialApp是基于mUi风格的，提供配置项
+    return MaterialApp(
+      // 标题
+      title: 'Flutter Demo',
+      // 主题
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // 首页
+      home: const MyHomePage(title: 'Flutter Demo Home Page ITry'),
+    );
+  }
+}
+
+// 动态的 在生命周期中持有状态，并根据状态更新UI
+class MyHomePage extends StatefulWidget {
+  // 常量 意味在编译时 就能确定值
+  // required 必须 this.title是必须的 如果缺失 会报错
+  const MyHomePage({super.key, required this.title});
+
+  //final 不可变的，将在类实例化的过程中被赋值，且生命周期中保持不变
+  final String title;
+
+  @override
+  // 创建相关联的状态对象
+  State<MyHomePage> createState() => _MyHomePageState();
+  // State<MyHomePage> createState() => _MyHomePageState();
+}
+
+// 关联的状态对象
+// 类名前的私有类 用下划线表示 不能在其他文件访问
+class _MyHomePageState extends State<MyHomePage> {
+  // 私有类
+  int _counter = 0;
+
+  String _myname = 'I Try it';
+
+// 增加数量
+  void _incrementCounter() {
+    setState(() {
+      // 状态改变
+      _counter++;
+      _myname = 'i can do it';
+    });
+  }
+
+// 减少数量
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      _myname = 'i can not   do it';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(widget.title)),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              const Text('You have pushed the button this many times'),
+              Text('$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium),
+              Text('$_myname', style: Theme.of(context).textTheme.labelMedium)
+            ])),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'incrementCount',
+              child: const Icon(Icons.add),
+            ),
+            const SizedBox(height: 16),
+            FloatingActionButton(
+                onPressed: _decrementCounter,
+                tooltip: 'decrementCount',
+                child: const Icon(Icons.remove))
+          ],
+        ));
+  }
+}
