36c128068f6f8c419a6188b5948c9d6775402548
S009497 <s009497@dgyiheda.com>
2024-03-11 17:07:36 +0800

    ①级联语法 .. ②??=赋值操作

--- helloWorld.dart
+++ helloWorld.dart
@@ -2,6 +2,21 @@
 
 import 'dart:io';
 
+class Person {
+  String name = ''; //添加初始化表达式 也可以用late关键字延迟字段的初始化时机
+  void run() {
+    print('$name is running');
+  }
+
+  void eat() {
+    print('$name is eating');
+  }
+
+  void sleep() {
+    print('$name is sleeping');
+  }
+}
+
 main(List<String> args) {
   print('HELLO World');
   String name = 'wwwwww';
@@ -67,4 +82,28 @@ main(List<String> args) {
 
   // 判断是否包含某个key or value
   print(numberMap.containsKey('age'));
+
+  // 函数 可赋值变量、可作为另一个函数or返回值使用
+  // 1.定义一个函数
+  // foo(String name) {
+  //   print('传入的name:$name');
+  // }
+
+  // ??=赋值操作
+  var name1 = 'codeWhy';
+  print(name1);
+  var name2 = null;
+  name2 ??= 'kobe';
+  // 当name2初始化null，赋值kobe
+  print(name2);
+
+  // 级联语法 ..
+  final p1 = Person();
+  p1.name = 'hhhh';
+  p1.run();
+
+  final p2 = Person()
+    ..name = 'sss'
+    ..run();
+  print(p2.name);
 }
